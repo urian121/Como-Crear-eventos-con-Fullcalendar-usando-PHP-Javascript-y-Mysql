7c71f86a578c7b9ff4788d7ddecc8e40b500a852
URIAN VIERA <urian1213viera@gmail.com>
2022-11-29 21:57:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Mi Calendario:: Ing. Urian Viera</title>
-	<link rel="stylesheet" href="">
 	<link rel="stylesheet" type="text/css" href="css/fullcalendar.min.css">
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
